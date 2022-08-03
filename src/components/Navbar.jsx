be3ae08126ee78909cb7df6c3babc001bfdbda80
Nurmansyah <nurmanchoy@gmail.com>
2022-08-03 21:30:36 +0700

    feat: ketika user login yang muncul button logout, begitu sebaliknya

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { keluarDariAplikasi } from "../authentication/firebase";
+import { auth, keluarDariAplikasi } from "../authentication/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 import "./Navbar.css";
 
 function Navbar() {
   const navigate = useNavigate();
+  const [user] = useAuthState(auth);
 
   const buttonLogoutClickHandler = async () => {
     await keluarDariAplikasi();
@@ -15,13 +17,10 @@ function Navbar() {
     <div className="navbar">
       <div className="container">
         <div className="left">
-          <img
-            src="https://img.icons8.com/color/48/000000/sunny-side-up-eggs.png"
-            alt="recipe logo"
-          />
+          <img src="../sunny-side-up-eggs.png" alt="recipe logo" />
         </div>
         <div className="button right" onClick={buttonLogoutClickHandler}>
-          Logout
+          {!user ? "Login" : "Logout"}
         </div>
       </div>
     </div>
